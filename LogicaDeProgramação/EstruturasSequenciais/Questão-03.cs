0fc25fde6268ce3b5311e38086816879bcd64ee8
Gustavo Maciel <gustavohcmaciel15@gmail.com>
2025-05-02 16:07:49 -0300

    Questão resolvida

---
+++ LogicaDeProgramação/EstruturasSequenciais/Questão-03.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace LogicaDeProgramacao
+{
+    class Questao_03
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            string nome_1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade_1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            string nome_2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade_2 = int.Parse(Console.ReadLine());
+
+            double idade_media = (idade_1 + idade_2) / 2.0;
+
+            Console.WriteLine($"A idade média de {nome_1} e {nome_2} é de {idade_media.ToString("F1", CI)}");
+        }
+    }
+}
