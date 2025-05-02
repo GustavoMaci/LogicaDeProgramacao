6d442e30610a78ebaf168463ec856a39516d55f4
Gustavo Maciel <gustavohcmaciel15@gmail.com>
2025-05-02 15:24:22 -0300

    Primeira questão

---
+++ LogicaDeProgramação/EstruturasSequenciais/Questão-01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace LogicaDeProgramacao
+{
+    class Questao_01
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a largura de um terreno: ");
+            double largura = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite o comprimento do terreno: ");
+            double comprimento = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite o valor do metro quadrado: ");
+            double valor = double.Parse(Console.ReadLine(), CI);
+
+            double area_terreno = largura * comprimento;
+            double preco = area_terreno * valor;
+
+            Console.WriteLine($"Area do terreno = {area_terreno.ToString("F2", CI)}");
+            Console.WriteLine($"Preco do terreno = {preco.ToString("F2", CI)}");
+        }
+    }
+}
---
+++ LogicaDeProgramação/EstruturasSequenciais/Questão-02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace LogicaDeProgramacao
+{
+    class Questao_02
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Base do retangulo: ");
+            double Base = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Altura do retangulo: ");
+            double Altura = double.Parse(Console.ReadLine(), CI);
+
+            double Area = Base * Altura;
+            double Perimetro = Base * 2 + Altura * 2;
+            double Diagonal = Math.Sqrt(Altura * Altura + Base * Base);
+
+            Console.WriteLine($"AREA = {Area.ToString("F4", CI)}");
+            Console.WriteLine($"Perimetro = {Perimetro.ToString("F4", CI)}");
+            Console.WriteLine($"DIAGONAL = {Diagonal.ToString("F4", CI)}");
+        }
+    }
+}
